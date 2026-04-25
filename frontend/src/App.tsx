37dfc7f72a382a6b71149e757496072468e5710c
Ugi <90718205+Ugi0@users.noreply.github.com>
2026-04-25 21:10:45 +0300

    Change favicon icon to a tokki emote

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router";
-import HomePage from "./pages/HomePage";
 import RoulettePage from "./pages/RoulettePage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/roulette" element={<RoulettePage />} />
+        <Route path="/" element={<RoulettePage />} />
       </Routes>
     </BrowserRouter>
   );
