b0d34a7be3a6615557c266cddd2c5d326bbede94
Ugi <90718205+Ugi0@users.noreply.github.com>
2026-04-25 21:12:13 +0300

    Set routing back

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import RoulettePage from "./pages/RoulettePage";
+import HomePage from "./pages/HomePage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<RoulettePage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/roulette" element={<RoulettePage />} />
       </Routes>
     </BrowserRouter>
   );
