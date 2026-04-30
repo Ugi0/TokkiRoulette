97891f8239a6a23a2fe79caec351f51c07761282
Luke Raykovitz <92956453+FreezeFyre@users.noreply.github.com>
2026-04-30 10:44:24 -0700

    Routed Analytics Page In Dev Branch
    Updated Analytics to the correct versioning from prior git,

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import RoulettePage from "./pages/RoulettePage";
+import RouletteAnalyticsPage from "./pages/RouletteAnalyticsPage.tsx";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<RoulettePage />} />
+          <Route path="/analytics" element={<RouletteAnalyticsPage />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/pages/RoulettePage.tsx
+++ frontend/src/pages/RoulettePage.tsx
@@ -3,6 +3,7 @@ import "./RoulettePage.css";
 import Footer from "../components/Footer";
 import { useState } from "react";
 import Notification from "../components/Notification";
+import {Link} from "react-router";
 
 export default function RoulettePage() {
   const [notification, setNotification] = useState<string | null>(null);
@@ -12,6 +13,7 @@ export default function RoulettePage() {
       <main className="roulette-page">
         <div className="roulette-page__header">
           <h1>Totally not rigged Roulette table</h1>
+            <Link to={"/analytics"}> Analytics </Link>
         </div>
 
         <RouletteTable setNotification={setNotification}  />
